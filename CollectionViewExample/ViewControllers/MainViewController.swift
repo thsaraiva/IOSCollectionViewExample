03758dce718228015dd6800ad00ff6919c444bed
Thiago Saraiva <thiago.saraiva@kwiff.com>
2018-08-14 15:26:55 +0100

    adding DetailViewController

--- CollectionViewExample/ViewControllers/MainViewController.swift
+++ CollectionViewExample/ViewControllers/MainViewController.swift
@@ -8,15 +8,16 @@
 
 import UIKit
 
-var collectionData = ["Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho",
+var collectionData = ["Janeiro", "Fevereiro", "Marco", "Abril", "maio", "Junho",
                       "Julho", "Agosto", "Setembro","Outubro", "Novembro", "Dezembro"]
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
     
     @IBOutlet private weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Meses"
         let width = (view.frame.size.width-20)/3
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         layout.itemSize = CGSize(width: width, height: width)
@@ -26,9 +27,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "DetailSegue"{
+            if let dest = segue.destination as? DetailViewController,
+                let firstSelectedIndex = collectionView.indexPathsForSelectedItems?.first{
+                dest.selection = collectionData[firstSelectedIndex.row]
+            }
+        }
+    }
 }
 
-extension ViewController: UICollectionViewDataSource{
+extension MainViewController: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return collectionData.count
     }
@@ -42,7 +52,7 @@ extension ViewController: UICollectionViewDataSource{
     }
 }
 
-extension ViewController : UICollectionViewDelegate{
+extension MainViewController : UICollectionViewDelegate{
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("Selected item: \(collectionData[indexPath.row])")
     }
