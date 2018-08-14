7e9d728a347c348a8a9b62b2332b56af6592931f
Thiago Saraiva <thiago.saraiva@kwiff.com>
2018-08-14 14:47:20 +0100

    App displaying months names in a collection view

--- CollectionViewExample/ViewController.swift
+++ CollectionViewExample/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var collectionData = ["Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho",
+                      "Julho", "Agosto", "Setembro","Outubro", "Novembro", "Dezembro"]
 
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let width = (view.frame.size.width-20)/3
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
+extension ViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return collectionData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath)
+        if let label = cell.viewWithTag(100) as? UILabel{
+            label.text = collectionData[indexPath.row]
+        }
+        return cell
+    }
+}
 
+extension ViewController : UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected item: \(collectionData[indexPath.row])")
+    }
 }
 
